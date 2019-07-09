3fb31e26faffce6f1d1828775315895a05cad5f2
moonnight5 <1696442886@qq.com>
2019-07-09 17:02:57 +0800

    add

---
+++ react-ant-router-demo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+import Layout from './page/Layout';
+import 'antd/dist/antd.css';
+// hashRouter  # hashChange
+// historyRouter 不带# BrowserRouter 实现方式是通过h5 的History api
+function App() {
+  return (
+    <Router>
+      <Route path="/" component={Layout} />
+    </Router>
+  );
+}
+
+export default App;
